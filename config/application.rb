7c649de71859b1668ecef99bab44853ffb063f13
kawasakikou <forte0000000000@yahoo.co.jp>
2014-08-21 15:04:12 +0900

    nothing .gitignore

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+require 'devise'
 
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,7 +21,6 @@ module ECScraping
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    
   end
 end
