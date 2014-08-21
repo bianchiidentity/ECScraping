a77684fa223cc517f54e604484b004445964b1f9
kuro_milk <fuj.t601216@gmail.com>
2014-08-21 16:32:40 +0900

    Revert "Kou/add_extract2_url"

--- app/models/scraping/scraping_from_all.rb
+++ app/models/scraping/scraping_from_all.rb
@@ -4,23 +4,18 @@ require 'open-uri'
 # Nokogiriライブラリの読み込み
 require 'nokogiri'
 
-class App::Models::Scraping::ScrapingFromAll
+class Tasks::Scraping::ScrapingFromAll
 # スクレイピング先のURL
   #getterとsetter
   attr_accessor :url
   #初期化
   def initialize
     @url = Listpage.all.first.url
-    # @tag = Listpage.link_get_tag
-    # @attribute = Listpage.attribute
-    # @value = Listpage.link_get_value
   end
 
 
   def execute
-    # @first_details = "//" + @tag + "[@class=" + cfItem + "]"
-    # @second_details = @tag + "[@class=" + deatiBtn + "]/" + a
-    
+
     begin
       raise NilUrlError if @url.nil?
 
@@ -36,12 +31,7 @@ class App::Models::Scraping::ScrapingFromAll
 
       #一覧ページからすべての商品の詳細ページへのURLを取得する
       doc.xpath('//div[@class="cfItem"]').each do |node|
-      p node.xpath('div[@class="deaitiBtn"]/a').attribute('href').value
-
-      doc.xpath(@first_details).each do |node|
-      return node.xpath(@second_details).attribute('href').value
-      
-
+        p node.xpath('div[@class="deaitiBtn"]/a').attribute('href').value
       end
     rescue => e
       puts e.message
@@ -54,3 +44,5 @@ class ScrapingFromAll::NilUrlError < RuntimeError
     "引数にURLを指定してください"
   end
 end
+
+
